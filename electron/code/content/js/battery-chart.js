2ef8c10f5de9fba63a9b3f38879da4d98fce04f3
ddialar <ddialar@gmail.com>
2016-06-21 21:52:12 +0100

    chore(battery-chart): Code edited in order to unify the bar width.

--- electron/code/content/js/battery-chart.js
+++ electron/code/content/js/battery-chart.js
@@ -1,33 +1,43 @@
 'use strict';
 
 var ctx = document.getElementById('battery-chart');
-var lightChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-        labels: ['Batt 1', 'Batt 2'],
-        datasets: [{
-            data: [3.7, 3.9],
-            backgroundColor: [
-              'rgba(54, 162, 235, 0.5)',
-              'rgba(54, 162, 235, 0.5)'
-            ],
-            borderColor: [
-              'rgba(54, 162, 235, 1)',
-              'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [{
-          ticks: {
-            beginAtZero: true,
-          }
-        }]
+var chartType = 'horizontalBar';
+var chartLabels = ['Batt 1', 'Batt 2'];
+var chartData = [3.7, 3.9];
+var chartDataSet = {
+    labels: chartLabels,
+    datasets: [{
+      data: chartData,
+      backgroundColor: [
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(54, 162, 235, 0.5)'
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
+    }]
+};
+var chartOptions = {
+  legend: {
+    display: false
+  },
+  scales: {
+    xAxes: [{
+      ticks: {
+        beginAtZero: true,
       }
-    }
+    }],
+    yAxes: [{
+      categoryPercentage: 0.18,
+      barPercentage: 1
+    }]
+  }
+};
+
+var batteryChart = new Chart(ctx, {
+    type: chartType,
+    data: chartDataSet,
+    options: chartOptions
 });
