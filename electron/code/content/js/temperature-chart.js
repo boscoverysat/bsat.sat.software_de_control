5405ac2a5d2a1d2ebe01de28ba4204f45f6b2eb1
ddialar <ddialar@gmail.com>
2016-06-21 21:52:44 +0100

    chore(temperature-chart): Code edited in order to unify the bar width.

--- electron/code/content/js/temperature-chart.js
+++ electron/code/content/js/temperature-chart.js
@@ -1,41 +1,92 @@
 'use strict';
 
 var ctx = document.getElementById('temperature-chart');
-var lightChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-        labels: ['Batt', 'Cockpit', 'Side 1', 'Side 2', 'Side 3', 'Side 4'],
-        datasets: [{
-            data: [12, -37, -55, -60, 5, 28],
-            backgroundColor: [
-              'rgba(54, 162, 235, 0.5)',
-              'rgba(54, 162, 235, 0.5)',
-              'rgba(54, 162, 235, 0.5)',
-              'rgba(54, 162, 235, 0.5)',
-              'rgba(54, 162, 235, 0.5)',
-              'rgba(54, 162, 235, 0.5)'
-            ],
-            borderColor: [
-              'rgba(54, 162, 235, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
+var chartType = 'horizontalBar';
+var chartLabels = ['Batt', 'Cockpit', 'Side 1', 'Side 2', 'Side 3', 'Side 4'];
+var chartData = [12, -37, -55, -60, 5, 28];
+var chartDataSet = {
+    labels: chartLabels,
+    datasets: [{
+      data: chartData,
+      backgroundColor: [
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(54, 162, 235, 0.5)'
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
+    }]
+};
+var chartOptions = {
+  legend: {
+    display: false
+  },
+  scales: {
+    yAxes: [{
+      categoryPercentage: 0.55,
+      barPercentage: 1,
+      ticks: {
+        beginAtZero:true
       }
-    }
+    }]
+  }
+};
+
+var temperatureChart = new Chart(ctx, {
+    type: chartType,
+    data: chartDataSet,
+    options: chartOptions
 });
+
+
+
+
+// var ctx = document.getElementById('temperature-chart');
+// var temperatureChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: {
+//         labels: ['Batt', 'Cockpit', 'Side 1', 'Side 2', 'Side 3', 'Side 4'],
+//         datasets: [{
+//             data: [12, -37, -55, -60, 5, 28],
+//             backgroundColor: [
+//               'rgba(54, 162, 235, 0.5)',
+//               'rgba(54, 162, 235, 0.5)',
+//               'rgba(54, 162, 235, 0.5)',
+//               'rgba(54, 162, 235, 0.5)',
+//               'rgba(54, 162, 235, 0.5)',
+//               'rgba(54, 162, 235, 0.5)'
+//             ],
+//             borderColor: [
+//               'rgba(54, 162, 235, 1)',
+//               'rgba(54, 162, 235, 1)',
+//               'rgba(54, 162, 235, 1)',
+//               'rgba(54, 162, 235, 1)',
+//               'rgba(54, 162, 235, 1)',
+//               'rgba(54, 162, 235, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//       legend: {
+//         display: false
+//       },
+//       scales: {
+//         yAxes: [{
+//           ticks: {
+//             beginAtZero:true
+//           }
+//         }]
+//       }
+//     }
+// });
