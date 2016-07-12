a293b9595d6fa3a321e554969a2259cf4c9762b1
ddialar <ddialar@gmail.com>
2016-07-12 06:59:38 +0100

    chore(logs): Trabajando en la captura y representación de mensajes enviados desde el satélite.

--- electron/code/content/components/logs/logs.js
+++ electron/code/content/components/logs/logs.js
@@ -9,22 +9,71 @@ angular
 
 function LogsService() {}
 
-function LogsController($scope, $timeout) {
+function LogsController() {
   var self = this;
-  self.scope = $scope;
-  self.timeout = $timeout;
 
-  self.xPosition = 10;
-  self.yPosition = 20;
-  self.zPosition = 30;
+  self.logsMessagesCollection = [];
+  self.logsCorrectCounter = 0;
+  self.logsWrongCounter = 0;
+  self.logsChart;
+  self.ctx = document.getElementById('logs-chart').getContext('2d');
+  self.chartType = 'doughnut';
+  self.chartLabels = ['Correct', 'Wrong'];
+  self.chartData = [self.logsCorrectCounter, self.logsWrongCounter];
+  self.chartDataSet = {
+      labels: self.chartLabels,
+      datasets: [{
+        data: self.chartData,
+        backgroundColor: [
+          'rgba(0, 255, 0, 0.5)',
+          'rgba(255, 0, 0, 0.5)'
+        ],
+        borderColor: [
+          'rgba(255, 255, 255, 1)',
+          'rgba(255, 255, 255, 1)'
+        ],
+        borderWidth: 3
+      }]
+  };
+  self.chartOptions = {
+    cutoutPercentage: 75,
+    animationSteps: 40,
+    legend: {
+      display: true,
+      position: 'bottom'
+    }
+  };
 
-  this.timeout(function() {
-    console.log('Timeout working');
-    self.xPosition += 1;
-    self.yPosition += 0.5;
-    self.zPosition += 0.3;
-  }, 5000);
+  self.createChart = function() {
+    // NOTE: Context configutarion in order to full width chart.
+    self.ctx.canvas.width = 300;
+    self.ctx.canvas.height = 300;
+    self.logsChart = new Chart(self.ctx, {
+      type: self.chartType,
+      data: self.chartDataSet,
+      options: self.chartOptions
+    });
+  };
 
+  self.updateLogsChart = function() {
+    self.logsChart.segments[0].value = self.logsCorrectCounter;
+    self.logsChart.segments[1].value = self.logsWrongCounter;
+    self.logsChart.update();
+  };
+
+  self.addCorrectMessage = function(message) {
+    self.logsMessagesCollection.unshift(message);
+    self.logsCorrectCounter++;
+    self.logsWrongCounter = 0;
+    self.updateLogsChart();
+  };
+
+  self.addWrongMessage = function() {
+    self.logsWrongCounter++;
+    self.updateLogsChart();
+  };
+
+  self.createChart();
 }
 
 function LogsSection() {
